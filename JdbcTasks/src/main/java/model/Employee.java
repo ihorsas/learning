89b440ba10c70e349347de2e9fd7464b5506d340
isas <isas@roku.com>
2023-09-24 11:30:34 +0300

    add jdbc tasks

---
+++ JdbcTasks/src/main/java/model/Employee.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Date;
+
+public class Employee {
+
+  private int id;
+  private String name;
+  private double salary;
+  private Date hireDate;
+  private String department;
+
+}
